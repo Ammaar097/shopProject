5ef838997266dcefc6703eb2c3814a37e030b8e8
Padetsuek  Jangburee <padetsuek.2543@gmail.com>
2023-07-09 21:47:58 +0700

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,8 +1,8 @@
 <?php if (!isset($_SESSION)) {
     session_start();
 }
-include_once('./service/auth.php');
-include_once('./database/Product.php');
+include_once('service/auth.php');
+include_once('database/Product.php');
 isLogin();
 $product = new Product();
 $lost = $product->fetchLost();
